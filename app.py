147d952c329f007c6fe906773ea26b566defcf71
Corey Kovach <coreykovach1@gmail.com>
2023-08-19 15:02:06 -0400

    Added map to main.html

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import environ
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -6,7 +7,7 @@ app = Flask(__name__)
 def show_index():
     """Index page"""
 
-    return render_template("main.html")
+    return render_template("main.html", GOOGLE_MAPS_API_KEY=environ.get("GOOGLE_MAPS_API_KEY"))
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)
