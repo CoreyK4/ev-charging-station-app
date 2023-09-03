1555ad038656b0f5ded70b72746437858b56823c
Corey Kovach <coreykovach1@gmail.com>
2023-09-03 19:46:53 -0400

    Updated sidebar title to be sticky

--- static/jsx/google_maps.jsx
+++ static/jsx/google_maps.jsx
@@ -17,15 +17,16 @@ function Sidebar(props) {
     }, []);
 
     return (
-        <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white overflow-y-scroll vh-100">
-            <div className="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+        <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white vh-100">
+            <div className="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom position-sticky">
                 <span className="fs-5 fw-semibold">Search Pane</span>
             </div>
-            <div className="list-group list-group-flush border-bottom scrollarea">
+            <div className="list-group list-group-flush border-bottom overflow-y-scroll scrollarea">
                 {Object.keys(props.markers).map(key => (
                     <a key={key} onClick={() => animateMarker(key)} className="list-group-item list-group-item-action py-3 lh-tight">
                         <div className="d-flex w-100 align-items-center justify-content-between">
                             <strong className="mb-1">{props.markers[key].title}</strong>
+                            {/* <i class="las la-heart la-2x"></i> */}
                             <i className="las la-charging-station la-3x"></i>
                         </div>
                         <div className="col-10 mb-1 small sidebarAddress"><p>{props.markers[key].addressLine1}</p><p>{props.markers[key].town}, {props.markers[key].stateOrProvince} {props.markers[key].postcode}</p></div>
